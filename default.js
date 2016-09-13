eded74527c05279407c192a56c84e36015534e27
movies-box <fozidap@stexsy.com>
2016-09-13 17:13:39 -0300

    player

--- default.js
+++ default.js
@@ -94,7 +94,9 @@ $(document).ready(function() {
       var trackers = "&tr=http://9.rarbg.com:2710/announce&tr=http://announce.torrentsmd.com:6969/announce&tr=http://bt.careland.com.cn:6969/announce&tr=http://explodie.org:6969/announce&tr=http://mgtracker.org:2710/announce&tr=http://tracker.best-torrents.net:6969/announce&tr=http://tracker.tfile.me/announce&tr=http://tracker.torrenty.org:6969/announce&tr=http://tracker1.wasabii.com.tw:6969/announce&tr=udp://9.rarbg.com:2710/announce&tr=udp://9.rarbg.me:2710/announce&tr=udp://coppersurfer.tk:6969/announce&tr=udp://exodus.desync.com:6969/announce&tr=udp://open.demonii.com:1337/announce&tr=udp://tracker.btzoo.eu:80/announce&tr=udp://tracker.istole.it:80/announce&tr=udp://tracker.openbittorrent.com:80/announce&tr=udp://tracker.prq.to/announce&tr=udp://tracker.publicbt.com:80/announce"
       $.each(data["torrents"], function(key, val) {
         var magnet = "magnet:?xt=urn:btih:" + val["hash"] + "&dn=" + data["title_long"] + trackers;
+				var imdbid = data["imdb_code"];
         popup.append("<p><a class='magnet-link' href='" + magnet + "'>Download (" + val["quality"] + ")</a></p>");
+				popup.append("<a href=\"https://embed.torrents-time.com/#source=" + magnet + "&imdbid="+ imdbid +"\">WATCH</a>")
 				popup.append("<div class=\"torrentsTime\" data-setup=\'{\"source\": \""+ magnet +"\", \"imdbid\": \""+ data["imdb_code"] + "\"}\'></div>");
 				})
       });

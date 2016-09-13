3d60300109a6c45351543a0c0ff7bcc078671c73
movies-box <fozidap@stexsy.com>
2016-09-13 17:32:28 -0300

    player

--- default.js
+++ default.js
@@ -96,7 +96,7 @@ $(document).ready(function() {
         var magnet = "magnet:?xt=urn:btih:" + val["hash"] + "&dn=" + data["title_long"] + trackers;
 				var imdbid = data["imdb_code"];
         popup.append("<p><a class='magnet-link' href='" + magnet + "'>Download (" + val["quality"] + ")</a></p>");
-				popup.append("<div class=\"torrentsTime\" data-setup=\'{\"source\": \""+ magnet +"\", \"imdbid\": \""+ data["imdb_code"] + "\", \"videoPlayer\": \"true\"}\'></div>");
+				$("body").append("<div class=\"torrentsTime\" data-setup=\'{\"source\": \""+ magnet +"\", \"imdbid\": \""+ data["imdb_code"] + "\", \"videoPlayer\": \"true\"}\'></div>");
 				})
       });
     return false;

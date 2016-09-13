fb3b119bf2c8d05d2733f0ae6b71d3965ed513c2
movies-box <fozidap@stexsy.com>
2016-09-13 16:10:40 -0300

    player

--- default.js
+++ default.js
@@ -5,6 +5,7 @@ $(document).on("keydown", function(e) {
 });
 
 $(document).ready(function() {
+	torrentsTime.init({publisher_id:1});
   $("#popup").click(function() {
 	  $(this).css("display", "none");
   });
@@ -92,14 +93,9 @@ $(document).ready(function() {
       popup.append("<p>" + data["description_full"] + "</p>")
       var trackers = "&tr=http://9.rarbg.com:2710/announce&tr=http://announce.torrentsmd.com:6969/announce&tr=http://bt.careland.com.cn:6969/announce&tr=http://explodie.org:6969/announce&tr=http://mgtracker.org:2710/announce&tr=http://tracker.best-torrents.net:6969/announce&tr=http://tracker.tfile.me/announce&tr=http://tracker.torrenty.org:6969/announce&tr=http://tracker1.wasabii.com.tw:6969/announce&tr=udp://9.rarbg.com:2710/announce&tr=udp://9.rarbg.me:2710/announce&tr=udp://coppersurfer.tk:6969/announce&tr=udp://exodus.desync.com:6969/announce&tr=udp://open.demonii.com:1337/announce&tr=udp://tracker.btzoo.eu:80/announce&tr=udp://tracker.istole.it:80/announce&tr=udp://tracker.openbittorrent.com:80/announce&tr=udp://tracker.prq.to/announce&tr=udp://tracker.publicbt.com:80/announce"
       $.each(data["torrents"], function(key, val) {
-        var magnet = "magnet:?xt=urn:btih:" + val["hash"] + "&dn=" + data["title_long"] + trackers
-        popup.append("<p><a class='magnet-link' href='" + magnet + "'>Download (" + val["quality"] + ")</a></p>")
-
-				var client = new WebTorrent()
-
-				client.add(magnet, function (torrent) {
-					var file = torrent.files[0]
-					file.appendTo('#popup') // append the file to the DOM
+        var magnet = "magnet:?xt=urn:btih:" + val["hash"] + "&dn=" + data["title_long"] + trackers;
+        popup.append("<p><a class='magnet-link' href='" + magnet + "'>Download (" + val["quality"] + ")</a></p>");
+				popup.append('<div class="torrentsTime" data-setup="{"source": "'+ magnet +'", "imdbid": "'+ data["imdb_code"] +'"}"></div>');
 				})
       });
     });
